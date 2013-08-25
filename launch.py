039eb1c35620874a00cab83654d0c8f1b98743e5
Ganz7 <ganzse7en@gmail.com>
2013-08-25 12:07:51 +0530

    Repo Update

--- launch.py
+++ launch.py
@@ -1,7 +1,7 @@
 '''
 File : Launch.py
 Author : Ganz7 (ganzse7en@gmail.com)
-Description : A basic irc bot. 
+Description : A basic irc bot.
 
 Todo: 1) Needs refactoring.
       2) Extend the features.
@@ -15,28 +15,28 @@ from random import randint
 from botRequest import greetingsReq
 from botReply import greetingsRep
 
-server = "irc.freenode.net"   
-channel = "#madyo"		#Rand Channel
+server = "irc.freenode.net"
+channel = "#madyo"		#Random Channel
 botnick = "Awwbot"
 hostname = "DeathStar"	#Blow it up
 servername = "Mordor"	#Oh no I didn't
 
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print "Connecting to:"+server
-irc.connect((server, 6667))                                                        
+irc.connect((server, 6667))
 irc.send("USER "+ botnick +" "+ hostname +" "+ servername +" :McLovin\n") #user authentication
 irc.send("NICK "+ botnick +"\n")                            #call me sexy
 #irc.send("\r\n")    				        #Gotta reg a name
-irc.send("JOIN "+ channel +"\n")        
+irc.send("JOIN "+ channel +"\n")
 
-while 1:    
-	text=irc.recv(2040)  
-   	print text   
-	if text.find('PING') != -1: 
+while 1:
+	text=irc.recv(2040)
+   	print text
+	if text.find('PING') != -1:
       		irc.send('PONG ' + text.split() [1] + '\r\n') #I'm alive! Godammit!
 
-      	if (text.find('PRIVMSG') != -1 and text.find('.freenode.net') == -1): 
-      		for word in greetingsReq.words: 
+      	if (text.find('PRIVMSG') != -1 and text.find('.freenode.net') == -1):
+      		for word in greetingsReq.words:
       			if (text.lower().find(word)!= -1 ):
 		      		t = str(text)
 			 	ind = t.index('!')
